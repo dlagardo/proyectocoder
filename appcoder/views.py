a08bf2c095683845a5c1c57d66f35b6458d31921
dlagardo <darioalbertolagardo@hotmail.com>
2022-03-27 12:42:38 -0300

    modificacion primera: playground I

--- appcoder/views.py
+++ appcoder/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def inicio(request):
+ dict_title={"title" :"inicio"}
+ return render(request,"appcoder/index.html",dict_title)
+def estudiantes(request):
+ return render(request,"appcoder/estudiantes.html")
+
+def profesores(request):
+ return render(request,"appcoder/profesores.html")
+ 
+def cursos(request):
+  return render(request,"appcoder/cursos.html")
+ 
+def entregables(request):
+  return render(request,"appcoder/entregables.html")
